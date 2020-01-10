2dde53de9d479224d7f165938ff9209407bcdc42
Justin <xhergz@gmail.com>
2020-01-10 13:10:35 -0500

    Added a new class for exception utilities and a method that will aggregate an exception message with all inner exceptions.

---
+++ HergBotUtilities/ExceptionUtilities/ExceptionUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HergBot.Utilities.ExceptionUtilities
+{
+    public class ExceptionUtilities
+    {
+        /// <summary>
+        /// Returns a single string of the exception and all inner exceptions.
+        /// </summary>
+        /// <param name="exception">The exception to traverse through</param>
+        /// <returns>A joined string of all the exceptions messages</returns>
+        public static string GetAllExceptionMessages(Exception exception)
+        {
+            if (exception == null)
+            {
+                return string.Empty;
+            }
+            if (exception.InnerException != null)
+            {
+                return string.Join(" ", exception.Message, GetAllExceptionMessages(exception.InnerException));
+            }
+            return exception.Message;
+        }
+    }
+}
---
+++ HergBotUtilities_Tests/ExceptionUtilities_Tests/ExceptionUtilities_Tests.cs
@@ -0,0 +1,40 @@
+﻿/*
+* PROJECT: HergBot Utilities
+* PROGRAMMER: Justin
+* FIRST VERSION: 2020-01-10
+*/
+
+using System;
+
+using NUnit.Framework;
+
+using HergBot.Utilities.ExceptionUtilities;
+
+namespace HergBotUtilities_Tests.ExceptionUtilities_Tests
+{
+    class ExceptionUtilities_Tests
+    {
+        private const string TEST_MESSAGE = "Message.";
+
+        [Test]
+        public void GetAllExceptionMessages_WithNullException_ReturnsEmptyString()
+        {
+            Assert.AreEqual(string.Empty, ExceptionUtilities.GetAllExceptionMessages(null));
+        }
+
+        [Test]
+        public void GetAllExceptionMessages_WithOneException_ReturnsMessage()
+        {
+            Exception ex = new Exception(TEST_MESSAGE);
+            Assert.AreEqual(TEST_MESSAGE, ExceptionUtilities.GetAllExceptionMessages(ex));
+        }
+
+        [Test]
+        public void GetAllExceptionMessages_WithMultipleExceptions_ReturnsJoinedString()
+        {
+            Exception inner = new Exception(TEST_MESSAGE);
+            Exception ex = new Exception(TEST_MESSAGE, inner);
+            Assert.AreEqual(string.Join(" ", TEST_MESSAGE, TEST_MESSAGE), ExceptionUtilities.GetAllExceptionMessages(ex));
+        }
+    }
+}
